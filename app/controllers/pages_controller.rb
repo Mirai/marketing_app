e5c0984d9a04ca67c4360f16b8df826acd804b56
Robert Miner <robertrminer@gmail.com>
2011-01-02 06:31:47 +0000

    Finished with basic layout and static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def more
+    @title = "Learn More"
   end
 
   def pricing
+    @title = "Pricing"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
